d74b138288072a445982b16df3856f6aa4c3774b
Nick A <nicolas.a.asanov@gmail.com>
2025-05-12 11:02:42 -0400

    init spring

---
+++ talkfish/src/main/java/org/naasanov/talkfish/TalkfishApplication.java
@@ -0,0 +1,13 @@
+package org.naasanov.talkfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkfishApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TalkfishApplication.class, args);
+  }
+
+}
---
+++ talkfish/src/test/java/org/naasanov/talkfish/TalkfishApplicationTests.java
@@ -0,0 +1,13 @@
+package org.naasanov.talkfish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkfishApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
